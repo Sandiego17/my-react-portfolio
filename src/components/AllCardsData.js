8da4a042b2f4ed13c57dc742bb9fa94fa0c44f7b
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-03-13 16:51:54 +0100

    feat: all cards data for projects component

---
+++ src/components/AllCardsData.js
@@ -0,0 +1,90 @@
+import projImg1 from '../assets/images/dispatch-hub-proj-img.png';
+import projImg2 from '../assets/images/quiz-app-proj-img.png';
+import projImg3 from '../assets/images/login-proj-img.png';
+import projImg4 from '../assets/images/contactus-proj-img.png';
+import projImg5 from '../assets/images/robot-friend-proj-img.png';
+import projImg6 from '../assets/images/e-commerce-proj-img.png';
+import projImg7 from '../assets/images/todo-list-app-proj-img.png';
+import projImg8 from '../assets/images/movie-app-proj-img.png';
+import projImg9 from '../assets/images/landing-page-proj-img.png';
+import stacImg1 from '../assets/images/html5-icon.jpg';
+import stacImg2 from '../assets/images/css3-icon.jpg';
+import stacImg3 from '../assets/images/bootstrap-icon.jpg';
+import stacImg4 from '../assets/images/tailwind-icon.png';
+import stacImg5 from '../assets/images/git-icon.jpg';
+import stacImg6 from '../assets/images/github-light-icon.png';
+import stacImg7 from '../assets/images/javascript-image.jpg';
+import stacImg8 from '../assets/images/typescript-icon.jpg';
+import stacImg9 from '../assets/images/react-light-icon.png';
+import stacImg10 from '../assets/images/vuejs-icon.png';
+
+
+export const projects = [
+  {
+    title: "Dispatch Hub Blog",
+    description: "Design & Development",
+    imgUrl: projImg1,
+    projectUrl: "https://dispatch-hub-blog-page.vercel.app/"
+  },
+  {
+    title: "Quiz App",
+    description: "Design & Development",
+    imgUrl: projImg2,
+    projectUrl: "https://science-nature-quiz-app.vercel.app/"
+  },
+  {
+    title: "SignIn Form",
+    description: "Design & Development",
+    imgUrl: projImg3,
+    projectUrl: "https://signin-form-roan.vercel.app/"
+  },
+  {
+    title: "ContactUs Component",
+    description: "Design & Development",
+    imgUrl: projImg4,
+    projectUrl: "https://contact-us-css-animation-fontawesome.vercel.app/"
+  },
+  {
+    title: "Robot Friend",
+    description: "Design & Development",
+    imgUrl: projImg5,
+    projectUrl: "https://robot-friend.vercel.app/"
+  },
+  {
+    title: "e-Commerce Website",
+    description: "Design & Development",
+    imgUrl: projImg6,
+    projectUrl: "https://e-commerce-website-mocha.vercel.app/"
+  },
+  {
+    title: "Todo List App",
+    description: "Design & Development",
+    imgUrl: projImg7,
+    projectUrl: "https://todo-list-app-vuejs.vercel.app/"
+  },
+  {
+    title: "Movie App",
+    description: "Design & Development",
+    imgUrl: projImg8,
+    projectUrl: "https://movie-app-teal-two.vercel.app/"
+  },
+  {
+    title: "Landing Page",
+    description: "Design & Development",
+    imgUrl: projImg9,
+    projectUrl: "https://simple-startup-landing-page.vercel.app/"
+  }
+];
+
+export const stacks = [
+  { toolUrl: stacImg1 },
+  { toolUrl: stacImg2 },
+  { toolUrl: stacImg3 },
+  { toolUrl: stacImg4 },
+  { toolUrl: stacImg5 },
+  { toolUrl: stacImg6 },
+  { toolUrl: stacImg7 },
+  { toolUrl: stacImg8 },
+  { toolUrl: stacImg9 },
+  { toolUrl: stacImg10 }
+];

abc004286c9e6525d44279ea36487938ffbb391b
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-03-15 15:05:50 +0100

    feat: socials links in navbar and footer activated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/svgs/logo.svg";
-import navIcon1 from "../assets/svgs/nav-icon1.svg";
-import navIcon2 from "../assets/svgs/nav-icon2.svg";
-import navIcon3 from "../assets/svgs/nav-icon3.svg";
+import navIcon1 from '../assets/svgs/linkedin.svg';
+import navIcon2 from '../assets/svgs/github.svg';
+import navIcon3 from '../assets/svgs/twitter.svg';
 
 export const Footer = () => {
   return (
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="social" /></a>
-              <a href="#"><img src={navIcon2} alt="social" /></a>
-              <a href="#"><img src={navIcon3} alt="social" /></a>
+            <a href="https://www.linkedin.com/in/henry-uzor-gmnse/"><img src={navIcon1} alt="linkedIn" /></a>
+              <a href="https://github.com/Sandiego17"><img src={navIcon2} alt="github" /></a>
+              <a href="https://twitter.com/sandiego1611"><img src={navIcon3} alt="x" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/svgs/logo.svg';
-import navIcon1 from '../assets/svgs/nav-icon1.svg';
-import navIcon2 from '../assets/svgs/nav-icon2.svg';
-import navIcon3 from '../assets/svgs/nav-icon3.svg';
+import navIcon1 from '../assets/svgs/linkedin.svg';
+import navIcon2 from '../assets/svgs/github.svg';
+import navIcon3 from '../assets/svgs/twitter.svg';
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -47,9 +47,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="social" /></a>
-              <a href="#"><img src={navIcon2} alt="social" /></a>
-              <a href="#"><img src={navIcon3} alt="social" /></a>
+              <a href="https://www.linkedin.com/in/henry-uzor-gmnse/"><img src={navIcon1} alt="linkedIn" /></a>
+              <a href="https://github.com/Sandiego17"><img src={navIcon2} alt="github" /></a>
+              <a href="https://twitter.com/sandiego1611"><img src={navIcon3} alt="x" /></a>
             </div>
             <HashLink to='#connect'>
               <button className="vvd"><span>Let's Connect</span></button>

ac09ed2e2ddf2d035d7a5a7fa722c69d792fc2b7
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-10-26 21:52:31 +0100

    feat: new project image added to projects list

--- src/components/AllCardsData.js
+++ src/components/AllCardsData.js
@@ -1,12 +1,13 @@
-import projImg1 from '../assets/images/dispatch-hub-proj-img.png';
-import projImg2 from '../assets/images/quiz-app-proj-img.png';
-import projImg3 from '../assets/images/login-proj-img.png';
-import projImg4 from '../assets/images/contactus-proj-img.png';
-import projImg5 from '../assets/images/robot-friend-proj-img.png';
-import projImg6 from '../assets/images/e-commerce-proj-img.png';
-import projImg7 from '../assets/images/todo-list-app-proj-img.png';
-import projImg8 from '../assets/images/movie-app-proj-img.png';
-import projImg9 from '../assets/images/landing-page-proj-img.png';
+import projImg1 from '../assets/images/dashboard-project.png';
+import projImg2 from '../assets/images/dispatch-hub-proj-img.png';
+import projImg3 from '../assets/images/quiz-app-proj-img.png';
+import projImg4 from '../assets/images/login-proj-img.png';
+import projImg5 from '../assets/images/contactus-proj-img.png';
+import projImg6 from '../assets/images/robot-friend-proj-img.png';
+import projImg7 from '../assets/images/e-commerce-proj-img.png';
+import projImg8 from '../assets/images/todo-list-app-proj-img.png';
+import projImg9 from '../assets/images/movie-app-proj-img.png';
+import projImg10 from '../assets/images/landing-page-proj-img.png';
 import stacImg1 from '../assets/svgs/html5.svg';
 import stacImg2 from '../assets/svgs/css3.svg';
 import stacImg3 from '../assets/svgs/bootstrap.svg';
@@ -22,57 +23,63 @@ import stacImg11 from '../assets/svgs/vue.svg';
 
 export const projects = [
   {
+    title: "AlphaTwelve Dashboard",
+    description: "This dashboard was built with Vanilla JS & Chart.js. Dashboard is mobile responsive and have a dark theme mode functionality. Sidebar is collapsible, with table data popping up a modal when clicked.",
+    imgUrl: projImg1,
+    projectUrl: "https://alphatwelve-dashboard.vercel.app/"
+  },
+  {
     title: "Dispatch Hub Blog",
     description: "Design & Development",
-    imgUrl: projImg1,
+    imgUrl: projImg2,
     projectUrl: "https://dispatch-hub-blog-page.vercel.app/"
   },
   {
     title: "Quiz App",
     description: "Design & Development",
-    imgUrl: projImg2,
+    imgUrl: projImg3,
     projectUrl: "https://science-nature-quiz-app.vercel.app/"
   },
   {
     title: "SignIn Form",
     description: "Design & Development",
-    imgUrl: projImg3,
+    imgUrl: projImg4,
     projectUrl: "https://signin-form-roan.vercel.app/"
   },
   {
     title: "ContactUs Component",
     description: "Design & Development",
-    imgUrl: projImg4,
+    imgUrl: projImg5,
     projectUrl: "https://contact-us-css-animation-fontawesome.vercel.app/"
   },
   {
     title: "Robot Friend",
     description: "Design & Development",
-    imgUrl: projImg5,
+    imgUrl: projImg6,
     projectUrl: "https://robot-friend.vercel.app/"
   },
   {
     title: "e-Commerce Website",
     description: "Design & Development",
-    imgUrl: projImg6,
+    imgUrl: projImg7,
     projectUrl: "https://e-commerce-website-mocha.vercel.app/"
   },
   {
     title: "Todo List App",
     description: "Design & Development",
-    imgUrl: projImg7,
+    imgUrl: projImg8,
     projectUrl: "https://todo-list-app-vuejs.vercel.app/"
   },
   {
     title: "Movie App",
     description: "Design & Development",
-    imgUrl: projImg8,
+    imgUrl: projImg9,
     projectUrl: "https://movie-app-teal-two.vercel.app/"
   },
   {
     title: "Landing Page",
     description: "Design & Development",
-    imgUrl: projImg9,
+    imgUrl: projImg10,
     projectUrl: "https://simple-startup-landing-page.vercel.app/"
   }
 ];

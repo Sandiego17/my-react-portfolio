2536beeb560c9682c4f0b1d82b9c4d379d2296ab
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-10-30 11:03:00 +0100

    feat: new projects images

--- src/components/AllCardsData.js
+++ src/components/AllCardsData.js
@@ -1,13 +1,15 @@
 import projImg1 from '../assets/images/dashboard-project.png';
-import projImg2 from '../assets/images/dispatch-hub-proj-img.png';
-import projImg3 from '../assets/images/quiz-app-proj-img.png';
-import projImg4 from '../assets/images/login-proj-img.png';
-import projImg5 from '../assets/images/contactus-proj-img.png';
-import projImg6 from '../assets/images/robot-friend-proj-img.png';
-import projImg7 from '../assets/images/e-commerce-proj-img.png';
-import projImg8 from '../assets/images/todo-list-app-proj-img.png';
-import projImg9 from '../assets/images/movie-app-proj-img.png';
-import projImg10 from '../assets/images/landing-page-proj-img.png';
+import projImg2 from '../assets/images/triple-helix-scibiz.png';
+import projImg3 from '../assets/images/triple-helix-nigeria.png';
+import projImg4 from '../assets/images/dispatch-hub-proj-img.png';
+import projImg5 from '../assets/images/quiz-app-proj-img.png';
+import projImg6 from '../assets/images/login-proj-img.png';
+import projImg7 from '../assets/images/contactus-proj-img.png';
+import projImg8 from '../assets/images/robot-friend-proj-img.png';
+import projImg9 from '../assets/images/e-commerce-proj-img.png';
+import projImg10 from '../assets/images/todo-list-app-proj-img.png';
+import projImg11 from '../assets/images/movie-app-proj-img.png';
+import projImg12 from '../assets/images/landing-page-proj-img.png';
 import stacImg1 from '../assets/svgs/html5.svg';
 import stacImg2 from '../assets/svgs/css3.svg';
 import stacImg3 from '../assets/svgs/bootstrap.svg';
@@ -29,57 +31,69 @@ export const projects = [
     projectUrl: "https://alphatwelve-dashboard.vercel.app/"
   },
   {
+    title: "Triple Helix SciBiz Conference",
+    description: "This website is for the Triple Helix Nigeria SciBiz Conference, an event designed to foster collaboration between academia, industry, and government to drive innovation and economic development in Nigeria.",
+    imgUrl: projImg2,
+    projectUrl: "https://thn-scibiz.onrender.com/"
+  },
+  {
+    title: "Triple Helix Nigeria",
+    description: "The Triple Helix Model in Nigeria is a collaboration among academia, industry, and government. Academic institutions generate knowledge, industry applies it commercially, and the government provides support through policies and funding.",
+    imgUrl: projImg3,
+    projectUrl: "https://www.triplehelixnigeria.org/"
+  },
+  {
     title: "Dispatch Hub Blog",
     description: "Design & Development",
-    imgUrl: projImg2,
+    imgUrl: projImg4,
     projectUrl: "https://dispatch-hub-blog-page.vercel.app/"
   },
   {
     title: "Quiz App",
     description: "Design & Development",
-    imgUrl: projImg3,
+    imgUrl: projImg5,
     projectUrl: "https://science-nature-quiz-app.vercel.app/"
   },
   {
     title: "SignIn Form",
     description: "Design & Development",
-    imgUrl: projImg4,
+    imgUrl: projImg6,
     projectUrl: "https://signin-form-roan.vercel.app/"
   },
   {
     title: "ContactUs Component",
     description: "Design & Development",
-    imgUrl: projImg5,
+    imgUrl: projImg7,
     projectUrl: "https://contact-us-css-animation-fontawesome.vercel.app/"
   },
   {
     title: "Robot Friend",
     description: "Design & Development",
-    imgUrl: projImg6,
+    imgUrl: projImg8,
     projectUrl: "https://robot-friend.vercel.app/"
   },
   {
     title: "e-Commerce Website",
     description: "Design & Development",
-    imgUrl: projImg7,
+    imgUrl: projImg9,
     projectUrl: "https://e-commerce-website-mocha.vercel.app/"
   },
   {
     title: "Todo List App",
     description: "Design & Development",
-    imgUrl: projImg8,
+    imgUrl: projImg10,
     projectUrl: "https://todo-list-app-vuejs.vercel.app/"
   },
   {
     title: "Movie App",
     description: "Design & Development",
-    imgUrl: projImg9,
+    imgUrl: projImg11,
     projectUrl: "https://movie-app-teal-two.vercel.app/"
   },
   {
     title: "Landing Page",
     description: "Design & Development",
-    imgUrl: projImg10,
+    imgUrl: projImg12,
     projectUrl: "https://simple-startup-landing-page.vercel.app/"
   }
 ];
